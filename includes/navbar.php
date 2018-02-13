dd4bae0c90ce41d8ee4ed1895b03cb6636eacd00
Dushy <dylangallin@gmail.com>
2018-02-14 00:47:29 +0100

    Add a link to create a new member

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,6 +2,7 @@
     <ul>
         <li><a href="./index.php">AdminPanel</a></li>
         <li><a href="./stats.php">Stats</a></li>
+        <li><a href="./addMember.php">Add member</a></li>
         <li><a href="./userDetails.php">User's details & Change max storage</a></li>
         <li><a href="./plansManagement.php">Plans management</a></li>
         <li><a href="./runCron.php">Run crons</a></li>
